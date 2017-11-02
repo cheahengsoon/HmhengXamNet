34c5cda23b027e55b6688f5e97f1da5dbcc68439
Marvin Heng <Marvin.Heng@avire-global.com>
2017-11-02 12:42:52 +0800

    Add project files.

---
+++ HmhengXamNet.Core/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HmhengXamNet
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnConnect.Clicked += SendUDP;
+        }
+
+        public void SendUDP(object sender, EventArgs e)
+        {
+            UdpClient ucpClient = new UdpClient();
+            IPAddress ipAddress = IPAddress.Parse(txtIPAddress.Text);
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, Int32.Parse(txtPort.Text));
+
+            Byte[] data = Encoding.ASCII.GetBytes(txtMessage.Text);
+            ucpClient.Send(data, data.Length, endPoint);
+        }
+    }
+}
